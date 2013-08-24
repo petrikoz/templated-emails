931a42d0137324f54da8e1a7c4ac2eba4d344769
Graham <admin@grahammcculloch.co.uk>
2013-08-24 15:05:26 +0100

    Corrected reference to custom user model

--- templated_emails/utils.py
+++ templated_emails/utils.py
@@ -8,6 +8,7 @@ from django.template.loader import render_to_string
 from django.utils.translation import get_language, activate
 from django.db import models
 from django.core.exceptions import ImproperlyConfigured
+from django.contrib.auth import get_user_model
 
 try:
     from celery.task import task
@@ -39,8 +40,8 @@ def send_templated_email(recipients, template_path, context=None,
         if it is users the system will change to the language that the
         user has set as theyr mother toungue
     """
-    recipient_pks = [r.pk for r in recipients if isinstance(r, settings.AUTH_USER_MODEL)]
-    recipient_emails = [e for e in recipients if not isinstance(e, settings.AUTH_USER_MODEL)]
+    recipient_pks = [r.pk for r in recipients if isinstance(r, get_user_model())]
+    recipient_emails = [e for e in recipients if not isinstance(e, get_user_model())]
     send = _send_task.delay if use_celery else _send
     send(recipient_pks, recipient_emails, template_path, context, from_email,
          fail_silently)
@@ -48,7 +49,7 @@ def send_templated_email(recipients, template_path, context=None,
 
 def _send(recipient_pks, recipient_emails, template_path, context, from_email,
           fail_silently):
-    recipients = list(settings.AUTH_USER_MODEL.objects.filter(pk__in=recipient_pks))
+    recipients = list(get_user_model().objects.filter(pk__in=recipient_pks))
     recipients += recipient_emails
 
     current_language = get_language()
@@ -64,7 +65,7 @@ def _send(recipient_pks, recipient_emails, template_path, context, from_email,
 
     for recipient in recipients:
         # if it is user, get the email and switch the language
-        if isinstance(recipient, settings.AUTH_USER_MODEL):
+        if isinstance(recipient, get_user_model()):
             email = recipient.email
             try:
                 language = get_users_language(recipient)
@@ -102,7 +103,7 @@ def _send(recipient_pks, recipient_emails, template_path, context, from_email,
         msg.send(fail_silently=fail_silently)
 
         # reset environment to original language
-        if isinstance(recipient, settings.AUTH_USER_MODEL):
+        if isinstance(recipient, get_user_model()):
             activate(current_language)
 if use_celery:
     _send_task = task(_send)
